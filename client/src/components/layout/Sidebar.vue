e4c670d550b1e716ce4c5c85f170841f7f19442e
joseareia <jose.apareia@gmail.com>
2023-03-25 19:03:49 +0000

    edit: dashboard, login and register page OK

---
+++ client/src/components/layout/Sidebar.vue
@@ -0,0 +1,190 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
+<template>
+    <div class="container-fluid">
+        <nav id="sidebarMenu" class="d-md-block sidebar collapse">
+            <div class="logo">
+                <router-link class="nav-link" id="nav-link-logo" :to="{ name: 'Dashboard' }">
+                    <span style="vertical-align">La Gavotte.</span>
+                </router-link>
+            </div>
+
+            <div class="position-sticky pt-3">
+                <ul class="nav flex-column" id="nav-flex-sidebar">
+                    <li class="nav-item nav-item-title">Navigation</li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :class="{
+                            active: $route.name === 'Dashboard'
+                            }" :to="{ name: 'Dashboard' }">
+                            <i class="bi bi-house-fill" style="position:relative;bottom:1.5px;"></i>
+                            Dashboard
+                        </router-link>
+                    </li>
+
+                    <li class="nav-item">
+                        <div class="d-flex justify-content-center">
+                            <a href="#" class="btn btn-secondary d-flex align-items-center justify-content-center btn-share">
+                                <i class="bi bi-github me-2"></i>
+                                Source Code
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+#nav-link-logo:hover {
+    background-color: #1f2937 !important;
+}
+
+#sidebarMenu {
+    width: 260px;
+    min-width: 260px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    background-color: #1f2937;
+    z-index: 2000;
+    box-shadow: 0 0 35px 0 rgba(154, 161, 171, .15) !important;
+}
+
+.logo {
+    height: 4.375rem;
+    width: 260px;
+    min-width: 260px;
+    position: fixed;
+    top: 0;
+    color: #fff !important;
+    text-decoration: none !important;
+}
+
+.logo .nav-link {
+    height: 4.375rem;
+    width: 260px;
+    font-size: 1.313rem;
+    font-weight: 800;
+    color: #fff !important;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.nav-link:hover {
+    color: #f2f4f6;
+    background-color: #374151;
+    border-radius: 5px;
+}
+
+.nav-link:hover>svg {
+    fill: #f2f4f6;
+}
+
+ul {
+    padding: 3rem .75rem;
+}
+
+.nav-item-title {
+    padding: 12px 20px;
+    letter-spacing: .05em;
+    pointer-events: none;
+    cursor: default;
+    font-size: .6875rem;
+    text-transform: uppercase;
+    color: #7e8d9f;
+    font-weight: 700;
+}
+
+ul> :first-child {
+    padding: 0 20px 12px 20px !important;
+}
+
+.nav-link {
+    display: block;
+    padding: 13px 20px;
+    font-size: .9375rem;
+    position: relative;
+    color: #ccc;
+    font-weight: 600;
+}
+
+.sidebar .nav-link.active,
+.sidebar .nav-link.active svg {
+    color: #f0bc74;
+    font-weight: 700;
+    fill: #f0bc74 !important;
+}
+
+.nav-link i {
+    display: inline-block;
+    line-height: 1.0625rem;
+    margin: -3px 10px 0 0;
+    font-size: 1.1rem;
+    vertical-align: middle;
+    width: 20px;
+}
+
+svg {
+    fill: #ccc;
+    transition: .15s ease-in-out;
+    display: inline-block;
+    line-height: 1.0625rem;
+    margin: -3px 10px 0 0;
+    font-size: 1.1rem;
+    vertical-align: middle;
+    width: 20px;
+}
+
+.text-gray-800,
+.text-gray-800:hover {
+    color: #1f2937 !important;
+}
+
+.badge-md {
+    padding: .25rem .4rem;
+}
+
+.badge {
+    font-size: 12px;
+    font-weight: 700;
+}
+
+.bg-secondary {
+    background-color: #f0bc74 !important;
+}
+
+.btn-share {
+    position: fixed;
+    padding: 10px 0;
+    bottom: 20px;
+    width: 210px;
+}
+
+.btn-secondary {
+    color: #1f2937;
+    font-weight: 700;
+    background-color: #f0bc74;
+    border-color: #f0bc74;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, .15), 0 1px 1px rgba(17, 24, 39, .075);
+}
+
+.btn-secondary:hover {
+    background-color: #eeb15d;
+    border-color: #eeb15d;
+}
+
+.btn-secondary:hover {
+    color: #1f2937;
+    background-color: #f2c689;
+}
+
+.btn-secondary:active {
+    color: #1f2937 !important;
+    background-color: #f0bc74 !important;
+    border-color: #f0bc74 !important;
+}
+</style>
---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,149 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="row">
+        <div class="col-12">
+            <div class="p-title-box mt-4">
+                <div>
+                    <h2 class="p-title">Welcome back, Alberto Ramusino!</h2>
+                    <p style="margin-top:13px;">Web analytics dashboard for all your Mikrotik devices.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-xl-5 col-lg-12">
+            <div class="row">
+                <div class="col-md-12 col-lg-6">
+                    <div class="card widget-flat">
+                        <div class="card-body">
+                            <div class="float-end">
+                                <i class="bi bi-router-fill card-icon"></i>
+                            </div>
+                            <h5 class="text-muted fw-normal mt-0">Routers</h5>
+                            <h3 class="mt-3 mb-3">261</h3>
+                            <p class="mb-0 text-muted">
+                                <span class="text-success me-2">
+                                    98%
+                                </span>
+                                <span class="text-nowrap">Active right now</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 col-lg-6">
+                    <div class="card widget-flat">
+                        <div class="card-body">
+                            <div class="float-end">
+                                <i class="bi bi-database-fill card-icon"></i>
+                            </div>
+                            <h5 class="text-muted fw-normal mt-0">Backups</h5>
+                            <h3 class="mt-3 mb-3">143</h3>
+                            <p class="mb-0 text-muted">
+                                <span class="text-success me-2">
+                                    100%
+                                </span>
+                                <span class="text-nowrap">Backups done</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-7 col-lg-7">
+            <div class="card card-h-100">
+                <div class="d-flex card-header justify-content-between align-items-center">
+                    <h4 class="header-title">Title here.</h4>
+                </div>
+                <div class="card-body pt-0">
+                    <p>Lorem ipsum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.widget-flat {
+    position: relative;
+    overflow: hidden;
+}
+
+.card {
+    border: none;
+    margin-bottom: 24px;
+    box-shadow: 0 0 35px 0 rgba(154, 161, 171, .15) !important;
+    border-radius: 3px;
+}
+
+.card-header {
+    margin-top: 0;
+    background-color: #fff;
+    border: 0;
+    margin-bottom: 0;
+    padding: 1.5rem;
+}
+
+.card-header .header-title {
+    text-transform: uppercase;
+    letter-spacing: .02em;
+    font-size: .9rem;
+    margin-top: 0;
+}
+
+.card-body {
+    padding: 1.5rem;
+}
+
+.card-icon {
+    color: #f0bc74;
+    font-size: 16px;
+    background-color: rgba(255, 240, 155, 0.25);
+    height: 40px;
+    width: 40px;
+    text-align: center;
+    line-height: 40px;
+    border-radius: 3px;
+    display: inline-block;
+}
+
+.text-muted {
+    color: #8a969c !important;
+}
+
+.mt-3 {
+    margin-top: 1.5rem !important;
+}
+
+.mb-3 {
+    margin-bottom: 1.5rem !important;
+}
+
+.me-2 {
+    margin-right: .75rem !important;
+}
+
+.text-success {
+    color: rgb(10, 207, 151) !important;
+}
+
+.text-danger {
+    color: rgb(250, 92, 124) !important;
+}
+
+#arrow-icons {
+    font-size: 14.4px;
+    margin: 0;
+}
+
+.bi {
+    margin: 0 !important;
+}
+
+.text-nowrap {
+    font-size: 14.4px;
+    color: rgb(138, 150, 156);
+}
+</style>
